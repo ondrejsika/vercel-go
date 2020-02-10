f3bb86e9f67382d3cf616d534e3981ceb6c79d7d
Ondrej Sika <ondrej@ondrejsika.com>
2020-02-10 12:21:42 +0100

    Add ListDomains method which returns struct ListDomainsResponse instead of raw response from resty

--- zeit_domain.go
+++ zeit_domain.go
@@ -1,9 +1,48 @@
 package zeit
 
-import "github.com/go-resty/resty/v2"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/go-resty/resty/v2"
+)
+
+type ListDomainsResponse struct {
+	Domains []struct {
+		Id                  string   `json:"id"`
+		Name                string   `json:"name"`
+		ServiceType         string   `json:"serviceType"`
+		NsVerifiedAt        int      `json:"nsVerifiedAt"`
+		TxtVerifiedAt       int      `json:"txtVerifiedAt"`
+		CdnEnabled          bool     `json:"cdnEnabled"`
+		CreatedAt           int      `json:"createdAt"`
+		BoughtAt            int      `json:"boughtAt"`
+		VerificationRecord  string   `json:"verificationRecord"`
+		Verified            bool     `json:"verified"`
+		Nameservers         []string `json:"nameservers"`
+		IntendedNameservers []string `json:"intendedNameservers"`
+		Creator             struct {
+			Id       string `json:"id"`
+			Username string `json:"username"`
+			Email    string `json:"email"`
+		} `json:"creator"`
+	} `json:"domains"`
+}
 
 func (api ZeitAPI) RawListDomains() (*resty.Response, error) {
 	return api.get("/v4/domains", map[string]string{})
+}
+
+func (api ZeitAPI) ListDomains() (*ListDomainsResponse, error) {
+	rawResp, err := api.RawListDomains()
+	if err != nil {
+		return nil, err
+	}
+	respBody := rawResp.Body()
+	var resp ListDomainsResponse
+	json.Unmarshal([]byte(respBody), &resp)
+	fmt.Println(resp)
+	return &resp, nil
 }
 
 func (api ZeitAPI) RawAddDomain(name string) (*resty.Response, error) {
